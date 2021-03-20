882d2b426b8ae63ba7d7bca1704c04dce1ffa659
AlexHappyCode <arpenacoding@gmail.com>
2021-03-20 22:04:13 +0000

    fixed seeding problem

--- application/server/seeds/user-seed.js
+++ application/server/seeds/user-seed.js
@@ -26,74 +26,67 @@ const accounts = [
     },
 ]
 
+seed();
 // create an account for each type of user
 // also creates a restaurant for the owner (no menu yet)
-function seedDB(accounts) {
-    return new Promise((resolve, reject) => {
+async function seed() {
+	for (const account of accounts) {
+		let accountRes = await accountModel.insertAccount(account);
 
-        accounts.forEach(async (account) => {
-            let accountRes = await accountModel.insertAccount(account);
+		if (account.userType === 'customer') {
+			let customer = {
+				name: "Alex",
+				accountId: accountRes.insertId
+			}
+			await customerModel.insertCustomer(customer);
 
-            if (account.userType === 'customer') {
-                let customer = {
-                    name: "Alex",
-                    accountId: accountRes.insertId
-                }
-                await customerModel.insertCustomer(customer);
+			// insert restaurant owner (make restaurant too)
+		} else if (account.userType === 'restaurantOwner') {
+			let owner = {
+				name: "Tina",
+				accountId: accountRes.insertId
+			}
+			let ownerRes = await restaurantOwnerModel.insertOwner(owner)
 
-                // insert restaurant owner (make restaurant too)
-            } else if (account.userType === 'restaurantOwner') {
-                let owner = {
-                    name: "Tina",
-                    accountId: accountRes.insertId
-                }
-                let ownerRes = await restaurantOwnerModel.insertOwner(owner)
+			// insert restaurant address
+			let address = {
+				line1: "829 Cheetos Dr.",
+				line2: "suite 8",
+				city: "San Francisco",
+				state: "California",
+				zipcode: 99999
+			}
+			let addressRes = await addressModel.insertAddress(address);
 
-                // insert restaurant address
-                let address = {
-                    line1: "829 Cheetos Dr.",
-                    line2: "suite 8",
-                    city: "San Francisco",
-                    state: "California",
-                    zipcode: 99999
-                }
-                let addressRes = await addressModel.insertAddress(address);
+			// create restaurant
+			let restaurant = {
+				name: "'Bob''s Burgers'",
+				desciption: "'Awesome burgers. Come get some!'",
+				priceRating: 2,
+				cuisine: "'American'",
+				ownerId: ownerRes.insertId,
+				addressId: addressRes.insertId,
+			}
+			await restaurantModel.insertRestaurant(restaurant);
 
-                // create restaurant
-                let restaurant = {
-                    name: "'Bob''s Burgers'",
-                    desciption: "'Awesome burgers. Come get some!'",
-                    priceRating: 2,
-                    cuisine: "'American'",
-                    ownerId: ownerRes.insertId,
-                    addressId: addressRes.insertId,
-                }
-                await restaurantModel.insertRestaurant(restaurant);
+			// insert delivery driver (link to restaurant)
+		} else if (account.userType === 'deliveryDriver') {
+			let restaurants = await restaurantModel.getAll();
+			console.log('restaurants from getAll()', restaurants);
 
-                // insert delivery driver (link to restaurant)
-            } else if (account.userType === 'deliveryDriver') {
-                let restaurants = await restaurantModel.getAll();
-                console.log('restaurants from getAll()', restaurants);
+			let deliveryDriver = {
+				name: "Tyrone",
+				accountId: accountRes.insertId,
+				restaurantId: restaurants[0].restaurantId,
+			}
 
-                let deliveryDriver = {
-                    name: "Tyrone",
-                    accountId: accountRes.insertId,
-                    restaurantId: restaurants[0].restaurantId,
-                }
-
-                await deliveryDriverModel.insertDriver(deliveryDriver);
-            }
-        });
-        console.log('resolving');
-        return resolve(true);
-    });
+			await deliveryDriverModel.insertDriver(deliveryDriver);
+		} // end of if
+	} // end of for loop
+	db.end((err) => {
+		if (err) {
+			return console.log('error closing db connection:' + err.message);
+		}
+		console.log('Database connection closed.');
+	});
 }
-seedDB(accounts).then(res => {
-    console.log('res:', res);
-    db.end((err) => {
-        if (err) {
-            return console.log('error closing db connection:' + err.message);
-        }
-        console.log('Database connection closed.');
-    });
-}).catch(err => console.log('errrrrorrrororororo', err))
