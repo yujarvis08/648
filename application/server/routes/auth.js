b7efaf2a7bd17f1e9f52d62b10193ad3257f6d1a
AlexHappyCode <arpenacoding@gmail.com>
2021-04-08 23:19:33 +0000

    fixed unit test to properly test bcrypt with a hashed password and cleaned up the errors

--- application/server/routes/auth.js
+++ application/server/routes/auth.js
@@ -8,15 +8,13 @@ router.post('/login', async (req, res) => {
     let credentials = req.body
     let user = await account.getAccountFromEmail(credentials.email);
     user = user[0];
-    //console.log(user);
+    console.log(user);
 
     if (user === undefined) {
-        console.log('User is undefined probably because the user does ' +
-        'not exist in the database');
         res.status(404).json( {msg: 'User not found'} );
     }
     try {
-        if (bcrypt.compare(user.password, credentials.password)) {
+        if (await bcrypt.compare(credentials.password, user.password)) {
             res.cookie('account_id', user.accountId);
             res.status(200).json({ msg: 'Logged in' });
         } else {
@@ -24,7 +22,7 @@ router.post('/login', async (req, res) => {
             res.status(403).json({ msg: 'Invalid credentials' });
         }
     } catch (error) {
-        console.log('Something went wrong with the server');
+        //console.log('Something went wrong with the server');
         console.log(error);
         if (!res.headersSent) {
             res.status(500).json({ msg: 'Internal error' });
--- application/server/tests/unit/login.test.js
+++ application/server/tests/unit/login.test.js
@@ -2,27 +2,27 @@ const request = require('supertest');
 const app = require('./../../server');
 
 // user exists in the database
-test('Testing login', async () => {
+test('Testing Login - user exists', async () => {
     await request(app).post('/api/auth/login')
     .send({
-        username: 'test',
-        email: 'owner1@restaurant.com',
-        password: 'test123'
+        username: 'firstTest',
+        email: 'customer@mail.com',
+        password: 'testpass'
     }).expect(200);
 });
 
 // user exists in the database but password is wrong
-test('Testing login', async () => {
+test('Testing login - password is wrong', async () => {
     await request(app).post('/api/auth/login')
     .send({
-        username: 'test',
+        username: 'secondTest',
         email: 'owner1@restaurant.com',
         password: 'wrongPassword'
-    }).expect(200);
+    }).expect(403);
 });
 
 // user does not exist
-test('Testing login', async () => {
+test('Testing login - user does not exist', async () => {
     await request(app).post('/api/auth/login')
     .send({
         username: 'nonexistant-user',
