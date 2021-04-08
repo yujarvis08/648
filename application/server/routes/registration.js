58dcbfc0e3c8467ee3de9d6734b1d6595ac9122c
AlexHappyCode <arpenacoding@gmail.com>
2021-04-08 17:56:05 +0000

    route inserts into account then inserts into owner to satisfy foreign kkey constraints

--- application/server/routes/registration.js
+++ application/server/routes/registration.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const account = require('../models/Account');
+const restaurantOwner = require('../models/RestaurantOwner.js);
 
 router.post('/restaurantOwner', (req, res) => {
+    const { email, password } = req.body;
+    registrationDetails = {email, password};
+    registrationDetails.userType = 'restaurantOwner';
+    // input -> { userType, email, password }
+    account.InsertAccount(registrationDetails);
+    id = account.lastInsertId;
+    const { firstName, lastName } = req.body;
+    const name = firstName + " " + lastName
+    owner = { name, id }
+    // input -> { name, accountId }
+    restaurantOwner.insertOwner(owner);
 });
 
 module.exports = router;
--- application/server/tests/unit/registration.test.js
+++ application/server/tests/unit/registration.test.js
@@ -4,7 +4,7 @@ const app = require('./../../server');
 test('Testing login', async () => {
     await request(app).post('/api/auth/login')
     .send({
-        userType: 'userType',
+        userType: 'restaurantOwner',
         name: 'testname',
         email: 'owner1@restaurant.com',
         password: 'test123'
