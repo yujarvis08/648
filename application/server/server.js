67a654db1f8cb66cf70498087e6ec7cd945e09f2
AlexHappyCode <arpenacoding@gmail.com>
2021-04-08 17:19:48 +0000

    added registration route

--- application/server/server.js
+++ application/server/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const searchRoutes = require('./routes/search');
+const registrationRoute = require('./routes/registration');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -13,6 +14,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.use('/api/search', searchRoutes);
+app.use('/api/registration', registrationRoute);
 
 app.get('/ping', function (req, res) {
     return res.json({ msg: 'pong' });
