031c7b369689a1f192b773a36830d6bc85697983
AlexHappyCode <arpenacoding@gmail.com>
2021-04-10 13:28:35 +0000

    added tests and endpoints for customer and deliveryDriver

--- application/server/routes/registration.js
+++ application/server/routes/registration.js
@@ -2,23 +2,58 @@ const express = require('express');
 const router = express.Router();
 const account = require('../models/Account');
 const restaurantOwner = require('../models/RestaurantOwner');
+const Customer = require('../models/Customer');
+const Driver = require('../models/DeliveryDriver');
 
+/* restaurantOwner registration */
 router.post('/restaurantOwner', async (req, res) => {
     const { email, password } = req.body;
     registrationDetails = {email, password};
     registrationDetails.userType = 'restaurantOwner';
-    console.log(registrationDetails);
-    console.log('beginning of route');
+
     // input -> { userType, email, password }
     await account.insertAccount(registrationDetails);
-    console.log('after inserAccount');
     accountId = await account.lastInsertId();
     const { name } = req.body;
-    console.log(req.body);
-    console.log(name);
     owner = { name, accountId }
+
     // input -> { name, accountId }
     restaurantOwner.insertOwner(owner);
+    res.status(200).json( {msg: 'registration complete'});
+});
+
+
+/* customer registration */
+router.post('/customer', async (req, res) => {
+    const { email, password } = req.body;
+    registrationDetails = {email, password};
+    registrationDetails.userType = 'customer';
+
+    // input -> { userType, email, password }
+    await account.insertAccount(registrationDetails);
+    accountId = await account.lastInsertId();
+    const { name } = req.body;
+    customer = { name, accountId }
+
+    // input -> { name, accountId }
+    Customer.insertCustomer(customer);
+    res.status(200).json( {msg: 'registration complete'});
+});
+
+/* driver registration */
+router.post('/driver', async (req, res) => {
+    const { email, password } = req.body;
+    registrationDetails = {email, password};
+    registrationDetails.userType = 'deliveryDriver';
+
+    // input -> { userType, email, password }
+    await account.insertAccount(registrationDetails);
+    accountId = await account.lastInsertId();
+    const { name, restaurantId } = req.body;
+    customer = { name, accountId, restaurantId }
+
+    // input -> { name, accountId }
+    Driver.insertDriver(customer);
     res.status(200).json( {msg: 'registration complete'});
 });
 
