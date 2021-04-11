69cddafc800249b5e49f8a0a9ce54a61b49414bd
yujarvis08 <yujarvis8@gmail.com>
2021-04-10 18:44:28 -0700

    Refactored Driver Registration on Homepage

--- application/client/src/components/Homepage.js
+++ application/client/src/components/Homepage.js
@@ -12,7 +12,7 @@ import Button from "react-bootstrap/Button";
 import logo from "../logo.svg";
 import banner from "../sfsubanner.png";
 import Modal from "react-bootstrap/Modal";
-import { Link } from "react-router-dom";
+import DriverRegistration from "./DriverRegistration";
 import RestaurantRegistration from "./RestaurantRegistration";
 
 // import { v4 as uuidv4 } from 'uuid';
@@ -314,107 +314,7 @@ const Homepage = () => {
             Registering as a driver is easy. Simply fill out the form below!
           </h3>
 
-          <Form style={{ textAlign: "left" }}>
-            <br></br>
-            <br></br>
-            <br></br>
-            <h3>Account Info</h3>
-
-            {/* Login modal if user already has account */}
-            <Form.Text>
-              Already have an Account?{" "}
-              <Link
-                variant="primary"
-                onClick={handleShow}
-                style={{
-                  textDecoration: "underline",
-                  color: "black",
-                }}
-              >
-                Click Here
-              </Link>
-              <Modal show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                  <Modal.Title>Login</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                  <Form>
-                    <Form.Group controlId="formBasicEmail">
-                      <Form.Label>Email address</Form.Label>
-                      <Form.Control type="email" placeholder="Enter email" />
-                    </Form.Group>
-
-                    <Form.Group controlId="formBasicPassword">
-                      <Form.Label>Password</Form.Label>
-                      <Form.Control type="password" placeholder="Password" />
-                      <Form.Text>
-                        Forgot Password? <a href="/">Click Here</a>
-                      </Form.Text>
-
-                      <Form.Text>
-                        Don't Have an Account?{" "}
-                        <a href="/userRegistration">Click Here</a>
-                      </Form.Text>
-                    </Form.Group>
-
-                    <Button variant="secondary" onClick={handleClose}>
-                      Close
-                    </Button>
-                    <Button
-                      variant="primary"
-                      type="submit"
-                      style={{ marginLeft: "325px" }}
-                    >
-                      Submit
-                    </Button>
-                  </Form>
-                </Modal.Body>
-              </Modal>
-            </Form.Text>
-            <br></br>
-
-            {/* Registration Form */}
-
-            <Form.Row>
-              <Form.Label>First Name</Form.Label>
-              <Form.Control type="firstname" placeholder="First Name" />
-            </Form.Row>
-            <br></br>
-            <Form.Row>
-              <Form.Label>Last Name</Form.Label>
-              <Form.Control type="lastname" placeholder="Last Name" />
-            </Form.Row>
-            <br></br>
-            <Form.Row>
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-            </Form.Row>
-            <br></br>
-            <Form.Row>
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
-            </Form.Row>
-            <br></br>
-            <Form.Row>
-              <Form.Label>Confirm Password</Form.Label>
-              <Form.Control type="password" placeholder="Confirm Password" />
-            </Form.Row>
-            <br></br>
-            <Form.Group controlId="exampleForm.SelectCustomSizeSm">
-              <Form.Label>Which restaurant do you work for?</Form.Label>
-              <Form.Control as="select" size="sm" custom>
-                <option>Bob's Burgers</option>
-                <option>Infinite Tacos</option>
-                <option>Pizzarino</option>
-                <option>Brain Freeze</option>
-                <option>Dynamic Coffee</option>
-              </Form.Control>
-            </Form.Group>
-            <br></br>
-            <Button variant="primary" type="submit">
-              Finish
-            </Button>
-          </Form>
+          <DriverRegistration />
 
           <br></br>
         </TabPanel>
