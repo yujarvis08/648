fc14b938124dcdfb9726efa44b8b42314b493ef6
Amit Joshi <66281851+amitkhalsa25@users.noreply.github.com>
2021-05-03 12:23:14 -0700

    4 pages completed.
    -Account Change Password
    - Checkout page (Done)
    - Account Info page done
    - Resturant menu (work in Progress)

--- application/client/src/App.js
+++ application/client/src/App.js
@@ -1,16 +1,24 @@
-import logo from './logo.svg';
-import { BrowserRouter, Route } from 'react-router-dom';
-import Main from "./components/Main";
-import UserRegistration from "./components/userRegistration";
+import Checkout from "./components/Checkout";
+import AccountInfo from './components/AccountInfo';
+// import { insertRestaurant } from '../../server/models/Restaurant';
+import Resturantmenu from './components/Resturantmenu';
+import AccountChangePassword from "./components/AccountChangePassword";
 
+ 
 function App() {
-  return (
-    <BrowserRouter>
-      <Main />
-      <Route path="/userRegistration" component={UserRegistration} />
-    </BrowserRouter>
-    
-  );
+ return (
+   <BrowserRouter>
+     <Main />
+     <Route path="/userRegistration" component={UserRegistration} />
+     <Route path="/Checkout" component={Checkout} />
+     <Route path="/AccountInfo" component={AccountInfo} />
+     <Route path="/Resturantmenu" component={Resturantmenu} />
+     <Route path="/AccountChangePassword" component={AccountChangePassword} />
+   </BrowserRouter>
+ 
+ 
+ );
 }
-
+ 
 export default App;
+
