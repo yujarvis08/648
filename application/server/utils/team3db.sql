41004e6af89d4b90c0c2ddd483e612ac801b768c
AlexHappyCode <arpenacoding@gmail.com>
2021-05-18 20:14:18 +0000

    put the column in restaurantOwner table instead of restaurant table - restaurantStatus Boolean

--- application/server/utils/team3db.sql
+++ application/server/utils/team3db.sql
@@ -63,6 +63,7 @@ CREATE TABLE IF NOT EXISTS `team3db`.`restaurantOwner` (
   `ownerId` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   `accountId` INT NOT NULL,
+  `restaurantStatus` BOOL DEFAULT False,
   PRIMARY KEY (`ownerId`),
   INDEX `accountIdFk_idx` (`accountId` ASC) VISIBLE,
   CONSTRAINT `ownerAccountIdFK`
@@ -112,7 +113,6 @@ CREATE TABLE IF NOT EXISTS `team3db`.`restaurant` (
   `priceRating` VARCHAR(3) NULL,
   `addressId` INT NULL,
   `imagePath` VARCHAR(255) NULL,
-  `restaurantStatus` BOOL DEFAULT False,
   INDEX `ownerIdFK_idx` (`ownerId` ASC) VISIBLE,
   PRIMARY KEY (`restaurantId`),
   INDEX `businessScheduleId_idx` (`businessScheduleId` ASC) VISIBLE,
