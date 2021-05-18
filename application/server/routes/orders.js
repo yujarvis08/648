8157e806be5374964ccc09e230bb8e77d76826a7
Roberto Herman <roberto.herman021@gmail.com>
2021-05-17 19:53:45 -0700

    Submit order and Order status
    - Orders can now be submitted and saved in DB
    - Orders can now be set to 'delivered' by the delivery driver

--- application/server/routes/orders.js
+++ application/server/routes/orders.js
@@ -1,18 +1,31 @@
 const express = require('express');
 const router = express.Router();
 const Order = require('../models/Order');
+const Address = require('../models/Address');
+const Customer = require('../models/Customer');
 
 //Create order: POST /orders/submit
 router.post('/submit', async (req, res) => {
-    console.log('in order submit')
-    let { cartItems, total } = req.body;
-    console.log('cart items and total in submit order:', cartItems, total);
-    let { account_id: accountId } = req.cookies;
+    let address = { line1, line2, city, state, zipcode, } = req.body;
+    const order = { total, instructions, cartItems } = req.body;
+    let accountId = req.cookies.account_id;
+    // insert address and get the addressId back
+    let result = await Address.insertAddress(address);
+    order.addressId = result.insertId;
+    // Use account ID from cookie to get customer ID
+    let accResult = await Customer.getCustomerId(accountId);
+    if (accResult) {
+        order.customerId = accResult[0].customerId;
+    } else {
+        console.log('No customer with that accountId found');
+        console.log('Perhaps it is a different type of User');
+    }
+    // get restaurantId from any menu item's menuId
     let key = Object.keys(cartItems)[0];
-    let result = await Order.getRestaurantIdFromMenuItemId(cartItems[key].menuItemId);
-    let restaurantId = result[0].restaurantId;
-    // TODO: CREATE A MODEL FUNCTION WHERE WE CAN PASS ALL ORDERS AND INSERT THEM ALL INTO RESTAURANTORDERS
-    await Order.insertOrder(accountId, restaurantId);
+    result = await Order.getRestaurantIdFromMenuItemId(cartItems[key].menuItemId);
+    order.restaurantId = result[0].restaurantId;
+    // finally insert order
+    await Order.insertOrder(order);
     res.status(200).json({ msg: 'created an order' });
 });
 
@@ -20,7 +33,6 @@ router.post('/submit', async (req, res) => {
 router.get('/getOrders', async (req, res) => {
     let { account_id: accountId } = req.cookies;
     let orders = await Order.getOrders(accountId);
-    console.log('ORDERS IN ROUTE', orders);
     res.status(200).json({
         msg: 'got the orders',
         orders: orders
@@ -49,10 +61,11 @@ router.get('/orderStatus', async (req, res) => {
 });
 
 // update the order status
-router.put('/setOrderStatus', async (req, res) => {
-    let { restaurantOrderId } = req.body;
-    await Order.setOrderStatus(restaurantOrderId);
-    res.status(200).json({ msg: 'Set the order status' });
+router.put('/setStatus', async (req, res) => {
+    console.log('in set status');
+    let { orderId, orderStatus } = req.body;
+    await Order.setStatus(orderId, orderStatus);
+    res.status(200).json({ msg: `Set the order status to ${orderStatus}.` });
 });
 
 module.exports = router;
